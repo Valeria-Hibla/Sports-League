d86d54e6cf6e863048513ce3b32d84064e023d6d
Valeria-Hibla <valeriarodriguez1127989@correo.itm.edu.co>
2026-05-11 23:28:53 -0500

    Program was edited

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
 builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>();
 builder.Services.AddScoped<ISponsorRepository, SponsorRepository>();
 builder.Services.AddScoped<ITournamentSponsorRepository, TournamentSponsorRepository>();
-
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
 
 // ── Services ──
@@ -30,7 +30,7 @@ builder.Services.AddScoped<IPlayerService, PlayerService>();
 builder.Services.AddScoped<IRefereeService, RefereeService>();          
 builder.Services.AddScoped<ITournamentService, TournamentService>();
 builder.Services.AddScoped<ISponsorService, SponsorService>();
-
+builder.Services.AddScoped<IMatchService, MatchService>();
 
 // ── AutoMapper ──
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
